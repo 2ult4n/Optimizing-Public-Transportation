c42093a6b12c6c548181da12c3fb084234940eef
2ult4n <isultanfx@gmail.com>
2021-03-26 15:55:41 +0300

    Faust stream processing server configured - transformation done

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -28,30 +28,36 @@ class TransformedStation(faust.Record):
     order: int
     line: str
 
+def transform(station: Station) -> TransformedStation:
+    line = ""
+    try: 
+        line ="red" if station.red else "blue" if station.blue else "green" if station.green else None
+        if line is None:
+            raise ValueError("Line cannot be None.")
+    except ValueError:
+        logger.error('Station lines were all false!')
+    transformed_station = TransformedStation(station.station_id, station.station_name, station.order, line)
+    return transformed_station
 
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+in_topic = app.topic("com.cta.analytics.db.stations", value_type=Station)
+out_topic = app.topic("com.cta.analytics.streams.stations", partitions=1)
+
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+table = app.Table(
+   "stations",
+   default=int,
+   partitions=1,
+   changelog_topic=out_topic,
+)
 
 
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+@app.agent(in_topic)
+async def db_station(stations):
+
+    async for station in stations:
+        transformedStation = transform(station)
+        await out_topic.send(key=transformedStation.station_name, value=transformedStation)
 
 
 if __name__ == "__main__":
